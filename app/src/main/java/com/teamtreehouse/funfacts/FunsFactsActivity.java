7376afab2e0058293bf4e546eb48d7db9fc3cdcb
bianet13 <bia.macedo13@gmail.com>
2015-08-27 12:26:29 -0300

    Initial commit

---
+++ app/src/main/java/com/teamtreehouse/funfacts/FunsFactsActivity.java
@@ -0,0 +1,47 @@
+package com.teamtreehouse.funfacts;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+public class FunsFactsActivity extends Activity {
+
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_funs_facts);
+
+        //Declare our View variables and assign them the Views from the layout files
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        showFactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                //Update a label with out dynamic fact
+                factLabel.setText(fact);
+
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        });
+    }
+
+
+}
